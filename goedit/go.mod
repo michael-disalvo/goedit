d46b188f0260ad396ade8645b6ee51142f4d490a
Michael DiSalvo <mtdisalvo3@gmail.com>
2024-11-23 15:38:19 -0500

    goedit: init termbox and display file

--- goedit/go.mod
+++ goedit/go.mod
@@ -4,6 +4,9 @@ go 1.22.2
 
 require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/michael-disalvo/gapbuf v0.0.0-00010101000000-000000000000 // indirect
 	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 )
+
+replace github.com/michael-disalvo/gapbuf => ../gapbuf
--- goedit/main.go
+++ goedit/main.go
@@ -1,32 +1,96 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"time"
 
-	runewidth "github.com/mattn/go-runewidth"
-	termbox "github.com/nsf/termbox-go"
+	"github.com/mattn/go-runewidth"
+	"github.com/michael-disalvo/gapbuf"
+	"github.com/nsf/termbox-go"
 )
 
-func tbprint(x, y int, msg string) {
-	for _, c := range msg {
-		termbox.SetCell(x, y, c, termbox.ColorRed, termbox.ColorDefault)
-		x += runewidth.RuneWidth(c)
+type EditSession struct {
+	buf      gapbuf.GapBuffer // the actual text backing
+	lineLens []int            // the length of each line, used to map (x, y) -> logical index
+	filename string           // name of the file we are editing
+}
+
+func (session *EditSession) display() {
+	x := 0
+	y := 0
+	for i := 0; i < session.buf.Len(); i++ {
+		ch := session.buf.Get(i)
+		if ch == '\n' {
+			y += 1
+			x = 0
+			continue
+		}
+		termbox.SetCell(x, y, ch, termbox.ColorWhite, termbox.ColorDefault)
+		x += runewidth.RuneWidth(ch)
 	}
 }
 
+func buildEditSession(filename string) (editSession EditSession, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file_reader := bufio.NewReader(file)
+	buf := gapbuf.NewGapBuffer()
+	lineLens := make([]int, 0)
+	currentLineLen := 0
+	for {
+		ch, _, ioErr := file_reader.ReadRune()
+		if ioErr != nil {
+			if ioErr == io.EOF {
+				lineLens = append(lineLens, currentLineLen)
+				break
+			} else {
+				err = fmt.Errorf("Error reading file: %w", ioErr)
+				return
+			}
+		}
+		currentLineLen += 1
+		if ch == '\n' {
+			lineLens = append(lineLens, currentLineLen)
+			currentLineLen = 0
+		}
+		buf.Push(ch)
+	}
+
+	editSession = EditSession{
+		buf,
+		lineLens,
+		filename,
+	}
+	return
+}
+
 func main() {
-	err := termbox.Init()
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	session, err := buildEditSession(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	tbprint(2, 2, "Hello Termbox!")
+	err = termbox.Init()
+	if err != nil {
+		fmt.Printf("Error initializing termbox: %w\n", err)
+		os.Exit(1)
+	}
+	defer termbox.Close()
+
+	session.display()
 	termbox.Flush()
-
-	time.Sleep(5 * time.Second)
-	termbox.Close()
-
+	time.Sleep(time.Second * 2)
 }
